d5e6076d1827fa6e23dc1eced88d7bd9020a8d1d
worrawit2022 <worrawit.king@gmail.com>
2024-03-03 15:43:45 +0700

    create and OAuth component

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,22 +1,30 @@
 import mongoose from 'mongoose';
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     username: {
-        type: String,
-        required: true,
-        unique: true,
+      type: String,
+      required: true,
+      unique: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true,
+      type: String,
+      required: true,
+      unique: true,
     },
     password: {
-        type: String,
-        required: true,        
-    }
-}, {timestamps:true}
+      type: String,
+      required: true,
+    },
+    avatar:{
+      type: String,
+      default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+    },
+  },
+  { timestamps: true }
 );
-const User = mongoose.model('User', userSchema)
+
+const User = mongoose.model('User', userSchema);
 
 export default User;
+//  "https://media.istockphoto.com/id/1327592506/vector/default-avatar-photo-placeholder-icon-grey-profile-picture-business-man.jpg?s=612x612&w=0&k=20&c=BpR0FVaEa5F24GIw7K8nMWiiGmbb8qmhfkpXcp1dhQg="
